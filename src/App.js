72bd7756ee38610bed8f6599522d3b6ebd6ff52b
millerkris <millerkv@mer.ci.nsu.ru>
2024-05-21 23:41:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+
+import RegistrationForm from './RegistrationForm';
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
+
